849ac1f0e6a523460ce9df925dd33b182b4b6032
farhit1 <farhit1@gmail.com>
2017-12-16 22:55:00 +0300

    .

--- lib/base.h
+++ lib/base.h
@@ -6,4 +6,6 @@ class IHeap {
     virtual void Insert(int key) = 0;
     virtual int GetMin() = 0;
     virtual void ExtractMin() = 0;
+    virtual void Meld(IHeap& other) = 0;
+    virtual ~IHeap() {} 
 };
